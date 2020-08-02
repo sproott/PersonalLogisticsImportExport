1ff09d39966618a4cbb137e6c046ea8ae1cb8938
David Hrabě <david.hrabe12@seznam.cz>
2020-08-02 17:32:15 +0200

    Initial Release

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PersonalLogisticsImportExport
 A mod for Factorio. Allows you to export your current personal logistics settings and import them in a different save.
+
+Heavily based on [QuickBar Import/Export by Therenas](https://mods.factorio.com/mod/quickbarimportexport), reused most of code and icons.
---
+++ data.lua
@@ -0,0 +1,60 @@
+local mod_name = "__PersonalLogisticsImportExport__"
+
+data:extend({
+  {
+    type = "shortcut",
+    name = "plie-import-pl",
+    order = "d[quickbar]-a[import]",
+    action = "lua",
+    localised_name = {"plie_shortcut.import-pl"},
+    icon = {
+        filename = mod_name .. "/icons/import-pl-x32.png",
+        priority = "extra-high-no-scale",
+        size = 32,
+        scale = 1,
+        flags = {"icon"}
+    },
+    small_icon = {
+        filename = mod_name .. "/icons/import-pl-x24.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 1,
+        flags = {"icon"}
+    },
+    disabled_small_icon = {
+        filename = mod_name .. "/icons/import-pl-x24-white.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 1,
+        flags = {"icon"}
+    }
+  }, 
+  {
+    type = "shortcut",
+    name = "plie-export-pl",
+    order = "d[quickbar]-b[export]",
+    action = "lua",
+    localised_name = {"plie_shortcut.export-pl"},
+    icon = {
+        filename = mod_name .. "/icons/export-pl-x32.png",
+        priority = "extra-high-no-scale",
+        size = 32,
+        scale = 1,
+        flags = {"icon"}
+    },
+    small_icon = {
+        filename = mod_name .. "/icons/export-pl-x24.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 1,
+        flags = {"icon"}
+    },
+    disabled_small_icon = {
+        filename = mod_name .. "/icons/export-pl-x24-white.png",
+        priority = "extra-high-no-scale",
+        size = 24,
+        scale = 1,
+        flags = {"icon"}
+    }
+  }
+})
